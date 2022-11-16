86967affedb38aa339b69976fa77090ea8236dbf
Ramachandran <jramachandran@NTS251918MC.local>
2022-11-16 17:12:16 +0530

    add parent

--- src/js/component/home/defaultPage.js
+++ src/js/component/home/defaultPage.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import { Card, Button, CardTitle, CardText, Row, Col, Collapse, CardBody } from 'reactstrap';
+import React from "react";
+import {
+  Card,
+  Button,
+  CardTitle,
+  CardText,
+  Row,
+  Col,
+  Collapse,
+  CardBody,
+} from "reactstrap";
 export default class DefaultPage extends React.Component {
-    render() {
-        return (
-            <div  style={{ padding: '30px' }}>
-                welcome
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div style={{ padding: "30px" }}>
+        welcome
+        <Footer />
+      </div>
+    );
+  }
 }
+
+const Parent = ({ children }) => {
+  return (
+    <div>
+      <h1>Heading</h1>
+      <p>Paragragh</p>
+      {children && children({ component: "Footer" })}
+    </div>
+  );
+};
+
+const withFooter = (Parent) => (props) => {
+  return (
+    <Parent {...props}>
+      {({component}) => {
+        return <h2>{component}</h2>;
+      }}
+    </Parent>
+  );
+};
+
+const Footer = withFooter(Parent);
